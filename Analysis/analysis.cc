bbc7e0ae7917940f893470377b4c90027813668e
Francesco Salamida <francesco.salamida@aquila.infn.it>
2020-05-21 16:50:16 +0200

    added plots of cross sectrions

--- Analysis/analysis.cc
+++ Analysis/analysis.cc
@@ -28,34 +28,44 @@ using namespace std;
 int np = 5;
 bool DEBUG = false;
 double lgEmin = 2.0; //100 MeV
-double lgEmax = 5.0; //100 GeV (in MeV)
+double lgEmax = 6.0; //100 GeV (in MeV)
 double step = 0.05; // energy step in logE
 int Nbins = (lgEmax-lgEmin)/step;
 
 void analysis(char* filename) {
 
   TH1D* hBR[np];
+  TH1D* hXsec[np];
   TH1D* hBRnorm
-    = new TH1D("nBRnorm", "total number of events vs energy", Nbins, lgEmin, lgEmax);
+    = new TH1D("hBRnorm", "total number of events vs energy", Nbins, lgEmin, lgEmax);
   hBRnorm->GetXaxis()->SetTitle("log_{10}(E/MeV)");
   hBRnorm->Sumw2();
+
+  TH1D* hXsecNominal
+    = new TH1D("hXsecNominal", "inelastic X-section vs energy ", Nbins, lgEmin, lgEmax);
+  hXsecNominal->GetXaxis()->SetTitle("log_{10}(E/MeV)");
+  hXsecNominal->GetYaxis()->SetTitle("#sigma_{inel}[mb]");
+  hXsecNominal->Sumw2();
+
   
   vector<int> xSec(np, 0);
   vector<int> first(np, 0);
   string products[] = {"C11", "B11", "Be10", "Be9", "Be7"};
   int Znuc[] = {6,  5,  4,  4, 4};
   int Anuc[] = {11, 11, 10, 9, 7};
+  int color[] = {kRed,  kAzure,  kSpring,  kOrange, kMagenta};
   
   TFile* fileIn= TFile::Open(filename);
   TTree* theTree=(TTree*)fileIn->Get("tree");
 
   int run, eventNumber, nSec, A, Z;
-  double primaryE;
+  double primaryE, inelXsec;
   TString* particleName = new TString();
   
   theTree->SetBranchAddress("Run",&run);
   theTree->SetBranchAddress("EventNumber",&eventNumber);
   theTree->SetBranchAddress("PrimaryE",&primaryE);
+  theTree->SetBranchAddress("inelXsec",&inelXsec);
   theTree->SetBranchAddress("nSec",&nSec);
   theTree->SetBranchAddress("ParticleName",&particleName);
   theTree->SetBranchAddress("A",&A);
@@ -75,6 +85,7 @@ void analysis(char* filename) {
     if(sectors == 0) {
       sectors = nSec;
       hBRnorm->Fill(log10(primaryE));
+      hXsecNominal->Fill(log10(primaryE),inelXsec);
       nev++;
     }
     
@@ -92,9 +103,20 @@ void analysis(char* filename) {
 	  sprintf(title, "BR[%s]",products[j].c_str());
 	  hBR[j]->GetYaxis()->SetTitle(title);
 	  hBR[j]->Sumw2();
+
+	  sprintf(name, "hXsec[%d]",j);
+	  sprintf(title, "x-section for %s",products[j].c_str());
+	  hXsec[j] = new TH1D(name, title, Nbins, lgEmin, lgEmax);
+	  hXsec[j]->GetXaxis()->SetTitle("log_{10}(E/MeV)");
+	  sprintf(title, "#sigma_{inel}[%s]",products[j].c_str());
+	  hXsec[j]->GetYaxis()->SetTitle(title);
+	  hXsec[j]->Sumw2();
+
+	  
 	  first[j] = 1;
 	}
 	hBR[j]->Fill(log10(primaryE));
+	hXsec[j]->Fill(log10(primaryE),inelXsec);
 	xSec[j]++;
       }
     }
@@ -105,13 +127,28 @@ void analysis(char* filename) {
   TCanvas* cBR = new TCanvas(1);
   cBR->cd();
   cBR->Divide(2,3);
+  TCanvas* cXsec = new TCanvas(1);
+  cXsec->cd();
+  cXsec->Divide(2,3);
+  
   cout << "Simulated events:" << nev << " (" << nev2+1 << ")" <<  endl;
   for (int i=0; i<np; i++) {
     cBR->cd(i+1); 
     if(DEBUG) cout << (double)xSec[i] << " / " << (double)nev << endl;
     hBR[i]->SetMarkerStyle(8);
+    hBR[i]->SetMarkerSize(.7);
+    hBR[i]->SetMarkerColor(color[i]);
+    hBR[i]->SetLineColor(color[i]);
     hBR[i]->Divide(hBRnorm);
     hBR[i]->Draw();
+    
+    cXsec->cd(i+1); 
+    hXsec[i]->SetMarkerStyle(4);
+    hXsec[i]->SetMarkerSize(.7);
+    hXsec[i]->SetMarkerColor(color[i]);
+    hXsec[i]->SetLineColor(color[i]);
+    hXsec[i]->Divide(hBRnorm);
+    hXsec[i]->Draw();
     
     if(DEBUG) for (int k=1; k<=Nbins; k++) cout << k << " " << hBR[i]->GetBinContent(k) << " " << hBR[i]->GetBinCenter(k) << endl;
   }
@@ -119,10 +156,16 @@ void analysis(char* filename) {
   if(DEBUG) {
     for (int k=1; k<=Nbins; k++) cout << k << " " << hBRnorm->GetBinContent(k) << " " << hBRnorm->GetBinCenter(k) << endl;
   }
+  
   cBR->cd(6);
   hBRnorm->Draw("H");
+  cBR->Update();
+
+  hXsecNominal->Divide(hBRnorm);
+  cXsec->cd(6);
+  hXsecNominal->Draw("H");
+  cXsec->Update();  
   
-  cBR->Update();  
  
 }
 
