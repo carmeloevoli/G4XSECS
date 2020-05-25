a4f59b3bb9f21f144735f504304e4f3743d73fd0
Carmelo Evoli <carmelo.evoli@gssi.it>
2020-05-25 12:48:39 +0200

    added INCL data

--- export.sh
+++ export.sh
@@ -1,4 +1,4 @@
-G4DIR=/Users/carmeloevoli/Work/libs/GEANT4
+G4DIR=/Users/carmeloevoli/Work/libs/geant4-data
 
 export G4ENSDFSTATEDATA=$G4DIR/G4ENSDFSTATE2.2
 export G4LEVELGAMMADATA=$G4DIR/PhotonEvaporation5.5
@@ -6,6 +6,7 @@ export G4SAIDXSDATA=$G4DIR/G4SAIDDATA2.0
 export G4NEUTRONXSDATA=$G4DIR/G4NEUTRONXS1.4
 export G4NEUTRONHPDATA=$G4DIR/G4NDL4.6
 export G4PARTICLEXSDATA=$G4DIR/G4PARTICLEXS2.1
+export G4INCLDATA=$G4DIR/G4INCL1.0
 
 FILE=$G4ENSDFSTATEDATA/ENSDFSTATE.dat
 if [[ ! -f "$FILE" ]]; then
@@ -35,4 +36,9 @@ fi
 FILE=$G4PARTICLEXSDATA/proton/inel1
 if [[ ! -f "$FILE" ]]; then
     echo "G4PARTICLEXSDATA does not exist"
+fi
+
+FILE=$G4INCLDATA/table_radius_hfb.dat
+if [[ ! -f "$FILE" ]]; then
+    echo "G4INCLDATA does not exist"
 fi
